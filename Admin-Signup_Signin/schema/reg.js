02454193dcab19c7bebf80f7b215009ee90ef459
Smit Patel <140514455+Smit-07-2003@users.noreply.github.com>
2023-11-01 13:45:48 +0530

    Add files via upload

--- Admin-Signup_Signin/schema/reg.js
+++ Admin-Signup_Signin/schema/reg.js
@@ -101,9 +101,35 @@ const Admin_acountloc_schema= new mongoose.Schema({
 })
 
 
+
+const Admin_forgotpass_schema= new mongoose.Schema({
+    
+    email: {
+        type: String,
+        unique : true,
+        trim: true
+    },
+    
+    link: {
+        type:String,
+        trim : true
+    },
+   
+    end_time: {
+        type:Date,
+        default: Date.now,
+        expires: 60 * 5, 
+    }
+    
+    
+})
+
+
+const Admin_forgotpass_link =new mongoose.model("Admin_forgotpass_link",Admin_forgotpass_schema);
+
 const Sign_up = new mongoose.model("playlist",Sign_up_Schema);
 const Signup_otp =new mongoose.model("Signup_otp",Sign_up_otp_Schema);
 const Signin_count =new mongoose.model("Signin_count",Resrtrict_signin_schema);
 const Adminaccountlock =new mongoose.model("Adminaccountlock",Admin_acountloc_schema);
-module.exports = {Sign_up,Signup_otp,Signin_count,Adminaccountlock}
+module.exports = {Sign_up,Signup_otp,Signin_count,Adminaccountlock,Admin_forgotpass_link}
 
