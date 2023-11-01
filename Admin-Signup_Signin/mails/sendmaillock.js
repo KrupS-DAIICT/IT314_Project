6b5341039b84395dbae960feb645279302d58dc2
Smit Patel <140514455+Smit-07-2003@users.noreply.github.com>
2023-11-01 13:43:33 +0530

    Add files via upload

--- Admin-Signup_Signin/mails/sendmaillock.js
+++ Admin-Signup_Signin/mails/sendmaillock.js
@@ -13,12 +13,44 @@ var transporter = nodemailer.createTransport({
 var mailOptions = {
   from: process.env.SENDER_EMAIL_ID,
   to: `${mail}`,
-  subject: 'Your Accout is lock',
-  html : `<p>Click on the link to unlock your account</p>
-              <br>
-              <h1>link will expire in 5 min</h1>
-              <br>
-              <a href="${resetLink}">link</a>`
+  subject: 'Your Accout is Locked',
+  html : `
+  <html>
+    <head>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f4f4f4;
+          text-align: center;
+        }
+        h1 {
+          color: #333;
+        }
+        p {
+          color: #555;
+        }
+        a {
+          display: inline-block;
+          margin: 10px;
+          padding: 10px 20px;
+          background-color: #007BFF;
+          color: #fff;
+          text-decoration: none;
+        }
+        a:hover {
+          background-color: #0056b3;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Account Locked</h1>
+      <p>Your account has been temporarily locked for security reasons.</p>
+      <p>To unlock your account, please click the following link:</p>
+      <a href="${resetLink}">Unlock My Account</a>
+    
+    </body>
+  </html>
+  `
 };
 
 transporter.sendMail(mailOptions, function(error, info){
